a106868f2962813fb4abb435e9979d764f44232e
Konrad Bystrzykowski <konor2000@gmail.com>
2021-06-02 13:47:06 +0200

    created save function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,8 +6,27 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    private $user;
+    private $password;
+
     public function generateView()
     {
-        return view('login');
+
+        return view('auth.login');
+    }
+
+    public function save(Request $req)
+    {
+        return $req->input();
+    }
+
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
     }
 }
