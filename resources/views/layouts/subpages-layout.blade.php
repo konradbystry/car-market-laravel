e4690440747b7b1fe2d37085d9144aeae72e7e1a
Konrad Bystrzykowski <konor2000@gmail.com>
2021-06-03 20:29:24 +0200

    changed stylesheet link to indirect

--- resources/views/layouts/subpages-layout.blade.php
+++ resources/views/layouts/subpages-layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="assets/images/cropped-favicon.png">
+    <link rel="icon" href={{env('APP_URL') . "/assets/images/cropped-favicon.png"}}>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href={{env('APP_URL') . "/assets/css/style.css"}}>
 
