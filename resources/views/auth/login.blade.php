6cd7cd5e142e7f8c379529dfd540d71613a7d4f8
Konrad Bystrzykowski <konor2000@gmail.com>
2021-06-03 20:24:55 +0200

    created ifs for login logout <a and login logout actions display

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,15 +3,19 @@
 @section('content')
 
     <div class="login">
-        <form class="pure-form pure-form-stacked" action={{route('auth.save')}} method="GET">
+        <form class="pure-form pure-form-stacked" action={{route('auth.check')}} method="GET">
             @csrf
 
 
 
-                <input type="user" id="stacked-user" name="user" placeholder="Login" />
+                <input type="user" id="stacked-user" name="user" placeholder="Login" value={{old('user')}} />
 
-                <input type="password" id="stacked-password" name="password" placeholder="Password" />
+                <input type="password" id="stacked-password" name="password" placeholder="Password"/>
                 <button type="submit" class="pure-button pure-button-primary">Sign in</button>
+
+                @if (@Session::get('fail'))
+                    <h2>fail</h2>
+                @endif
 
         </form>
     </div>
