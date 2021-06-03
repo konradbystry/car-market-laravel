c7bfa838648ddbb26497b2a4685a465f0f2445ee
Konrad Bystrzykowski <konor2000@gmail.com>
2021-06-03 20:20:04 +0200

    created logout function

--- app/Http/Controllers/CarMarketController.php
+++ app/Http/Controllers/CarMarketController.php
@@ -4,13 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Car;
+use App\Models\User;
 
 class CarMarketController extends Controller
 {
     public function index()
     {
+        $user = User::where('id_user', '=', session('LoggedUser'))->first();
         $cars = Car::all();
 
-        return view('CarMarket', ['cars' => $cars]);
+        return view('CarMarket', [
+            'cars' => $cars,
+            'user' => $user
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->session()->flush();
+        return redirect('/');
     }
 }
