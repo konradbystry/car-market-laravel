8dd183e2764f263963a454ffe1c90ad6fe7f71ba
Konrad Bystrzykowski <konor2000@gmail.com>
2021-06-09 17:53:16 +0200

    created many to many relationship

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Car extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_car');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function cars()
+    {
+        return $this->belongsToMany(Car::class, 'user_car');
+    }
 }
