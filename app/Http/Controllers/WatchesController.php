2c9c55956c6996cbfb2c16f18e87b764803fb333
Konrad Bystrzykowski <konor2000@gmail.com>
2021-06-14 22:39:41 +0200

    created display system for users watching ads

--- app/Http/Controllers/WatchesController.php
+++ app/Http/Controllers/WatchesController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+
 
 class WatchesController extends Controller
 {
     public function generateView()
     {
-        return view('watches');
+        $user = User::where('id', '=', session('LoggedUser'))->first();
+        //dd($user->cars);
+        return view('watches', [
+            'user' => $user
+        ]);
     }
 }
--- resources/views/watches.blade.php
+++ resources/views/watches.blade.php
@@ -22,6 +22,28 @@
                     </div>
                 </div>
             </a>
+
+            @foreach ($user->cars as $cars)
+            <a href="#">
+                <div class="content">
+                    <div class="photo">
+                        <img src="http://localhost/car-market-laravel/public/assets/images/test-photo.jpg" alt="">
+                    </div>
+                    <div class="description">
+                        <h2>{{$cars->brand}} {{$cars->model}}</h2>
+                        <ul>
+                            <li>{{$cars->production_date}}</li>
+                            <li>{{$cars->distance}} km</li>
+                            <li>enigne</li>
+                        </ul>
+                    </div>
+                    <div class="price">
+                        <h2>{{$cars->price}} pln</h3>
+                    </div>
+                </div>
+            </a>
+            @endforeach
+
         </div>
     </section>
 
